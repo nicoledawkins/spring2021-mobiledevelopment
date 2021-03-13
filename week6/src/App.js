c24f6f48ffbdeebbe28795f6fc1b74138422aca7
nicoledawkins <60334809+nicoledawkins@users.noreply.github.com>
2021-03-12 22:33:50 -0500

    Portfolio 1

---
+++ week6/src/App.js
@@ -0,0 +1,9 @@
+import Example from './Components/Example/index'
+
+function App() {
+  return (
+    <Example/>
+  );
+}
+
+export default App;
---
+++ week6/src/Components/Example/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Example extends React.Component {
+    render(){
+    
+       return (
+       <div>
+           <h1 style={{color: "red"}}>Hi, my name is Nicole!</h1>
+           <p>A Favortie Poem</p>
+           <p>The Two-Headed Calf
+               by: Laura Gilpin
+           </p>
+           <p>Tomorrow when the farm boys find this
+               freak of nature, they will wrap his body
+               in newspaper and carry him to the museum.
+           </p>
+           <p>But tonight he is alive and in the north
+               field with his mother. It is a perfect
+               summer evening: the moon rising over
+               the orchard, the wind in the grass. And
+               as he stares into the sky, there are
+               twice as many stars are usual.
+           </p>
+        </div>);
+    }
+}
+export default Example;
